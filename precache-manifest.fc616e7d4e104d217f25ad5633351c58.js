e476121b2c8ca6ee7034ed55f65a7d05057443b0
DeepshikaS <ddeepu13@gmail.com>
2020-05-20 23:32:21 +0530

    Updates

---
+++ precache-manifest.fc616e7d4e104d217f25ad5633351c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb912bf4771073e2459a2c6dee4e398",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "574401309d59062b72b3",
+    "url": "/react-todo-list/static/css/main.8686d987.chunk.css"
+  },
+  {
+    "revision": "ba423c3d78838e77ce39",
+    "url": "/react-todo-list/static/js/2.1836bc5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.1836bc5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574401309d59062b72b3",
+    "url": "/react-todo-list/static/js/main.0bb68bff.chunk.js"
+  },
+  {
+    "revision": "15e00161899a7e034a22",
+    "url": "/react-todo-list/static/js/runtime-main.86dff119.js"
+  }
+]);
